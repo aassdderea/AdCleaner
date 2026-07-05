099230b741c9202ed7811d0920917f4ee5c648f0
luofeng123123 <38125652+luofeng123123@users.noreply.github.com>
2026-07-05 12:40:14 +0800

    Update AdCleaner.m

--- AdCleaner.m
+++ AdCleaner.m
@@ -2,43 +2,86 @@
 #import <UIKit/UIKit.h>
 #import <objc/runtime.h>
 
-static IMP _orig_addSubview = NULL;
-static int _blockedCount = 0;
+static int _blocked = 0;
 
-static void _hook_addSubview(id self, SEL _cmd, UIView *view) {
-    NSString *cn = NSStringFromClass([view class]);
-    NSArray *keywords = @[
-        @"BUSplash", @"BUNativeExpress", @"BURewarded", @"BUFullscreen",
-        @"GDTSplash", @"GDTUnifiedNative", @"GDTUnifiedInterstitial",
-        @"GADBanner", @"GADNative", @"GADInterstitial", @"GADRewarded",
-        @"KSAdSplash", @"KSNative", @"KSReward",
-        @"UnityAds", @"VungleAd", @"AdColonyAd", @"ALSdk",
-        @"FBAdView", @"FBInterstitial", @"SigmobSplash",
-        @"MTGReward", @"MTGNative", @"IronSource"
+// === 广告类名前缀（更精确） ===
+static BOOL isAdClass(NSString *cn) {
+    static NSArray *pfx = nil;
+    if (!pfx) pfx = @[
+        // 穿山甲 Pangle
+        @"BUSplashAdView", @"BUNativeExpress", @"BUFullscreen",
+        @"BURewarded", @"BUAd",
+        // 优量汇 GDT
+        @"GDTSplashAd", @"GDTUnifiedBanner", @"GDTUnifiedInterstitial",
+        @"GDTUnifiedNative", @"GDTUnifiedRewardAd",
+        // AdMob
+        @"GADBannerView", @"GADNativeAd", @"GADInterstitial",
+        @"GADRewarded", @"GADNativeExpress",
+        // 快手
+        @"KSAdSplash", @"KSNativeAd", @"KSReward",
+        // 其他
+        @"UnityAds", @"VungleAd", @"FBAdView",
+        @"SigmobSplash", @"MTGReward", @"AdColony"
     ];
-    BOOL isAd = NO;
-    for (NSString *kw in keywords) {
-        if ([cn rangeOfString:kw].location != NSNotFound) { isAd = YES; break; }
+    for (NSString *p in pfx) {
+        if ([cn hasPrefix:p] || [cn rangeOfString:p].location != NSNotFound) return YES;
     }
-    if (isAd) { _blockedCount++; return; }
-
-    if (_orig_addSubview) {
-        ((void(*)(id, SEL, UIView *))_orig_addSubview)(self, _cmd, view);
-    }
+    return NO;
 }
 
-__attribute__((constructor))
-static void init(void) {
-    Method m = class_getInstanceMethod([UIView class], @selector(addSubview:));
-    if (m) {
-        _orig_addSubview = method_getImplementation(m);
-        method_setImplementation(m, (IMP)_hook_addSubview);
+// === Hook: addSubview ===
+static IMP _orig_addSubview = NULL;
+static void _h_addSubview(id s, SEL c, UIView *v) {
+    if (isAdClass(NSStringFromClass([v class]))) { _blocked++; return; }
+    if (_orig_addSubview) ((void(*)(id,SEL,UIView*))_orig_addSubview)(s,c,v);
+}
+
+// === Hook: insertSubview:atIndex: ===
+static IMP _orig_insertAtIndex = NULL;
+static void _h_insertAtIndex(id s, SEL c, UIView *v, NSInteger i) {
+    if (isAdClass(NSStringFromClass([v class]))) { _blocked++; return; }
+    if (_orig_insertAtIndex) ((void(*)(id,SEL,UIView*,NSInteger))_orig_insertAtIndex)(s,c,v,i);
+}
+
+// === Hook: insertSubview:aboveSubview: ===
+static IMP _orig_insertAbove = NULL;
+static void _h_insertAbove(id s, SEL c, UIView *v, UIView *sv) {
+    if (isAdClass(NSStringFromClass([v class]))) { _blocked++; return; }
+    if (_orig_insertAbove) ((void(*)(id,SEL,UIView*,UIView*))_orig_insertAbove)(s,c,v,sv);
+}
+
+// === Hook: UIWindow.makeKeyAndVisible ===
+static IMP _orig_makeKeyAndVisible = NULL;
+static void _h_makeKeyAndVisible(id s, SEL c) {
+    // 检查是否是广告窗口
+    UIWindow *w = (UIWindow *)s;
+    id rvc = w.rootViewController;
+    NSString *vcName = rvc ? NSStringFromClass([rvc class]) : @"";
+    if (isAdClass(vcName)) {
+        _blocked++;
+        w.hidden = YES;
+        return;
     }
+    if (_orig_makeKeyAndVisible) ((void(*)(id,SEL))_orig_makeKeyAndVisible)(s,c);
+}
 
-    NSString *docs = [NSSearchPathForDirectoriesInDomains(NSDocumentDirectory, NSUserDomainMask, YES) firstObject];
-    [@"v2 OK" writeToFile:[docs stringByAppendingPathComponent:@"adcleaner.log"]
-            atomically:YES encoding:NSUTF8StringEncoding error:nil];
+// === Hook: presentViewController ===
+static IMP _orig_presentVC = NULL;
+static void _h_presentVC(id s, SEL c, UIViewController *vc, BOOL anim, id cb) {
+    if (isAdClass(NSStringFromClass([vc class]))) { _blocked++; return; }
+    if (_orig_presentVC) ((void(*)(id,SEL,UIViewController*,BOOL,id))_orig_presentVC)(s,c,vc,anim,cb);
+}
 
+// === Swizzle 工具 ===
+static void swizzle(Class cls, SEL sel, IMP newImp, IMP *store) {
+    Method m = class_getInstanceMethod(cls, sel);
+    if (!m) return;
+    if (store) *store = method_getImplementation(m);
+    method_setImplementation(m, newImp);
+}
+
+// === Toast ===
+static void showToast(const char *ver, int blocked) {
     dispatch_after(dispatch_time(DISPATCH_TIME_NOW, 2 * NSEC_PER_SEC),
                    dispatch_get_main_queue(), ^{
         UIWindow *kw = nil;
@@ -56,13 +99,35 @@ static void init(void) {
         if (!kw) return;
 
         UILabel *lb = [[UILabel alloc] initWithFrame:CGRectMake(0, 100, kw.bounds.size.width, 50)];
-        lb.text = @"AdCleaner v2 已激活";
+        lb.text = [NSString stringWithFormat:@"AdCleaner %s | %d条拦截", ver, blocked];
         lb.textColor = [UIColor greenColor];
         lb.textAlignment = NSTextAlignmentCenter;
-        lb.font = [UIFont boldSystemFontOfSize:20];
+        lb.font = [UIFont boldSystemFontOfSize:16];
         lb.backgroundColor = [[UIColor blackColor] colorWithAlphaComponent:0.85];
         [kw addSubview:lb];
-        dispatch_after(dispatch_time(DISPATCH_TIME_NOW, 4 * NSEC_PER_SEC),
+        dispatch_after(dispatch_time(DISPATCH_TIME_NOW, 5 * NSEC_PER_SEC),
                        dispatch_get_main_queue(), ^{ [lb removeFromSuperview]; });
+    });
+}
+
+__attribute__((constructor))
+static void init(void) {
+    swizzle([UIView class], @selector(addSubview:), (IMP)_h_addSubview, &_orig_addSubview);
+    swizzle([UIView class], @selector(insertSubview:atIndex:), (IMP)_h_insertAtIndex, &_orig_insertAtIndex);
+    swizzle([UIView class], @selector(insertSubview:aboveSubview:), (IMP)_h_insertAbove, &_orig_insertAbove);
+    swizzle([UIWindow class], @selector(makeKeyAndVisible), (IMP)_h_makeKeyAndVisible, &_orig_makeKeyAndVisible);
+    swizzle([UIViewController class], NSSelectorFromString(@"presentViewController:animated:completion:"),
+            (IMP)_h_presentVC, &_orig_presentVC);
+
+    NSString *docs = [NSSearchPathForDirectoriesInDomains(NSDocumentDirectory, NSUserDomainMask, YES) firstObject];
+    [@"v3 OK" writeToFile:[docs stringByAppendingPathComponent:@"adcleaner.log"]
+            atomically:YES encoding:NSUTF8StringEncoding error:nil];
+
+    showToast("v3", _blocked);
+
+    // 定时更新Toast显示拦截计数
+    dispatch_after(dispatch_time(DISPATCH_TIME_NOW, 8 * NSEC_PER_SEC),
+                   dispatch_get_main_queue(), ^{
+        showToast("v3", _blocked);
     });
 }
