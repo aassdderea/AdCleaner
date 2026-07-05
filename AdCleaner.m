f5d57532439fbcf0764772eec42276201625977d
luofeng123123 <38125652+luofeng123123@users.noreply.github.com>
2026-07-05 12:33:33 +0800

    Update AdCleaner.m

--- AdCleaner.m
+++ AdCleaner.m
@@ -1,12 +1,42 @@
 #import <Foundation/Foundation.h>
 #import <UIKit/UIKit.h>
+#import <objc/runtime.h>
+
+static IMP _orig_addSubview = NULL;
+static int _blockedCount = 0;
+
+static void _hook_addSubview(id self, SEL _cmd, UIView *view) {
+    NSString *cn = NSStringFromClass([view class]);
+    NSArray *keywords = @[
+        @"BUSplash", @"BUNativeExpress", @"BURewarded", @"BUFullscreen",
+        @"GDTSplash", @"GDTUnifiedNative", @"GDTUnifiedInterstitial",
+        @"GADBanner", @"GADNative", @"GADInterstitial", @"GADRewarded",
+        @"KSAdSplash", @"KSNative", @"KSReward",
+        @"UnityAds", @"VungleAd", @"AdColonyAd", @"ALSdk",
+        @"FBAdView", @"FBInterstitial", @"SigmobSplash",
+        @"MTGReward", @"MTGNative", @"IronSource"
+    ];
+    BOOL isAd = NO;
+    for (NSString *kw in keywords) {
+        if ([cn rangeOfString:kw].location != NSNotFound) { isAd = YES; break; }
+    }
+    if (isAd) { _blockedCount++; return; }
+
+    if (_orig_addSubview) {
+        ((void(*)(id, SEL, UIView *))_orig_addSubview)(self, _cmd, view);
+    }
+}
 
 __attribute__((constructor))
 static void init(void) {
-    NSLog(@"[AdCleaner] 加载成功");
+    Method m = class_getInstanceMethod([UIView class], @selector(addSubview:));
+    if (m) {
+        _orig_addSubview = method_getImplementation(m);
+        method_setImplementation(m, (IMP)_hook_addSubview);
+    }
 
     NSString *docs = [NSSearchPathForDirectoriesInDomains(NSDocumentDirectory, NSUserDomainMask, YES) firstObject];
-    [@"OK" writeToFile:[docs stringByAppendingPathComponent:@"adcleaner.log"]
+    [@"v2 OK" writeToFile:[docs stringByAppendingPathComponent:@"adcleaner.log"]
             atomically:YES encoding:NSUTF8StringEncoding error:nil];
 
     dispatch_after(dispatch_time(DISPATCH_TIME_NOW, 2 * NSEC_PER_SEC),
@@ -26,7 +56,7 @@ static void init(void) {
         if (!kw) return;
 
         UILabel *lb = [[UILabel alloc] initWithFrame:CGRectMake(0, 100, kw.bounds.size.width, 50)];
-        lb.text = @"AdCleaner 已激活";
+        lb.text = @"AdCleaner v2 已激活";
         lb.textColor = [UIColor greenColor];
         lb.textAlignment = NSTextAlignmentCenter;
         lb.font = [UIFont boldSystemFontOfSize:20];
